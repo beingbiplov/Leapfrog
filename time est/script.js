fb6f0c93e484bc862d8a5c41351c415966a0abb2
beingbiplov <sharmabiplov@gmail.com>
2022-06-01 14:31:44 +0545

    design colors section

---
+++ time est/script.js
@@ -0,0 +1,8 @@
+
+
+const toogleBtn = document.getElementsByClassName('ham-icon')[0]
+const navLinks = document.getElementsByClassName('navlist')[0]
+
+toogleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active')
+})
