c49afe33c11c40b662451819aa4fd923d2ddb212
beingbiplov <sharmabiplov@gmail.com>
2022-06-14 18:35:53 +0545

    generate enemy cars #assignment9

--- assignments/JS Assignment/Assignment 10/assets/js/script.js
+++ assignments/JS Assignment/Assignment 10/assets/js/script.js
@@ -2,16 +2,45 @@ let toPx = (n) => {
     return `${n}px`
 }
 
+
+let getRandomFromList = (list) =>  {
+    return list[Math.floor(Math.random() * list.length)];
+  }
+
+const carList = ['car1.png', 'car2.png', 'car3.png']
+
+// generate random int 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 class CarGame{
-    constructor(carGameClass, lanes){
+    constructor(carGameClass, lanes, speed, carSpeed, carList){
         this.carGameClass = carGameClass
         this.lanes = lanes
         this.game
         this.userCarDiv
         this.userCar
-        this.userCarLane = 0
+        this.carHeight = 115
+        this.userCarLane = 1
+        this.speed = speed
+        this.carList = carList
+        this.carSpeed = carSpeed
+        this.enemyCarList = []
+
+        this.enemyCarMoveInterval
+        this.enemyCarCreateInterval
+
         this.carControls()
         this.userCarCreate()
+        this.enemyCarCreate()
+        this.enemyCarMove()
+    }
+
+    getPosX(lane){
+        return (lane * (600 / (this.lanes)))
     }
 
     userCarCreate(){
@@ -19,6 +48,7 @@ class CarGame{
 
         this.userCarDiv = document.createElement('div')
         this.userCarDiv.style.width = toPx(600 / (this.lanes)) 
+        this.userCarDiv.style.height = toPx(this.carHeight)
         this.userCarDiv.style.position = 'absolute'
         this.userCarDiv.style.bottom = '2%'
 
@@ -30,6 +60,8 @@ class CarGame{
         this.userCar.style.width = '55%'
         this.userCar.style.display = 'block'
         this.userCar.style.margin = '0 auto'
+
+        this.userCarDiv.style.left = toPx(this.getPosX(1))
 
         this.userCarDiv.appendChild(this.userCar)
         
@@ -49,20 +81,91 @@ class CarGame{
     moveCar(dir) {
         if ((this.userCarLane == 0 && dir == -1) || (this.userCarLane == this.lanes-1 && dir == +1)){
             // pass
-            console.log('pass')
         }
         else {
-            console.log(this.userCarLane)
+            // console.log(this.userCarLane)
             this.userCarLane += dir
-            let newPos = this.userCarLane * (600 / (this.lanes))
+            let newPos = this.getPosX(this.userCarLane) // * (600 / (this.lanes))
             this.userCarDiv.style.left = toPx(newPos)
             
         }
     }
+
+    enemyCarCreate(){
+        this.game = document.getElementsByClassName(this.carGameClass)[0]
+
+        this.enemyCarCreateInterval = setInterval(() =>{
+            // console.log(this.enemyCarList.length)
+            
+            
+            if(this.enemyCarList.length <=2){
+                let lane = getRandomInt(0,3)
+                const checkEnemyCarSpawn = this.checkEnemyCarSpawnPositon(lane)
+                // console.log(checkEnemyCarSpawn)
+                if (checkEnemyCarSpawn == true){
+                    const enemyCarDiv = document.createElement('div')
+                    enemyCarDiv.style.width = toPx(600 / (this.lanes)) 
+                    enemyCarDiv.style.position = 'absolute'
+                    enemyCarDiv.style.top = '2%'
+                    enemyCarDiv.style.height = toPx(this.carHeight)
+
+                    this.game.appendChild(enemyCarDiv)
+
+                    const enemyCar = document.createElement('img')
+                    
+                    enemyCar.src = `./assets/images/${getRandomFromList(this.carList)}`
+                    enemyCar.style.width = '55%'
+                    enemyCar.style.display = 'block'
+                    enemyCar.style.margin = '0 auto'
+                    enemyCar.style.transform = "rotate(180deg)";
+
+                    enemyCarDiv.style.left = toPx(this.getPosX(lane))
+                    enemyCarDiv.appendChild(enemyCar)
+
+                    this.enemyCarList.push({'car':enemyCarDiv, 'pos': 0, 'lane':lane})
+                    // this.enemyCarMove()
+                }
+                
+            }
+            // else if (this.enemyCarList.length > 3){
+            //     console.log('clear')
+            //     clearInterval(this.enemyCarCreateInterval)
+            //     setTimeout(this.enemyCarCreate(), 2000)
+            // }
+            }, this.speed * 150)
+        
+    }
+
+    checkEnemyCarSpawnPositon(lane){
+            // check conditions
+            return true
+        }
     
+    enemyCarMove(){
+
+        this.enemyCarMoveInterval = setInterval(() => {
+            if (this.enemyCarList.length > 0){
+                    // console.log(`speed: ${this.speed}`)
+                    this.enemyCarList.forEach((carDict, i) => {
+                        
+                        const newCarPos = carDict.pos + this.carSpeed
+                        this.enemyCarList[i]['pos'] = newCarPos
+                        carDict.car.style.top = toPx(newCarPos)
+
+                        if (this.enemyCarList[i]['pos'] > 580){
+                            this.enemyCarList[i]['car'].remove()
+                            this.enemyCarList.splice(i, 1)
+                        
+                        }
+                    })
+            
+                
+            }
+            }, this.speed )         
+    }
 }
 
 
 let carGameClass = 'car_game'
 
-const game1 = new CarGame(carGameClass, 3)
+const game1 = new CarGame(carGameClass, 3, 30, 2, carList)
